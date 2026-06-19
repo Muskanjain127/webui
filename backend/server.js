d83815dea104af6fca495e307e2e2dd67a2e9bd2
Muskanjain127 <146107376+Muskanjain127@users.noreply.github.com>
2026-06-20 04:20:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({origin:'http://localhost:5173'}));
+app.use(cors({origin:'http://localhost:5173','https://webui-wfv9.onrender.com'}));
 app.use(express.json());
 
 // Router
